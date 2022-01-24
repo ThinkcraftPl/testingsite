e659de94bc2910560f6ba50a12923561a9e2ec83
ThinkcraftPl <36745138+ThinkcraftPl@users.noreply.github.com>
2022-01-24 10:43:53 +0100

    planet gen start - three.js working

---
+++ src/routes/planetgen.svelte
@@ -0,0 +1,12 @@
+<div class="main_wrapper">
+	<div style="background-color:yellow">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta, exercitationem!</div>
+	<div style="background-color:aqua"></div>
+</div>
+<style>
+	.main_wrapper{
+		display: grid;
+		grid-column-gap:0;
+		grid-template-rows: 100%;
+		grid-template-columns: 20% 80%;
+	}
+</style>
